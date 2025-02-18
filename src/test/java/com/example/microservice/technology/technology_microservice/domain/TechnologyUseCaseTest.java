bc99227eddb924314a8a0df0ce93c925687d0d0d
Laura Jaimes <laura.jaimes@correounivalle.edu.co>
2025-02-18 17:17:40 -0500

    test implemented

--- src/test/java/com/example/microservice/technology/technology_microservice/domain/TechnologyUseCaseTest.java
+++ src/test/java/com/example/microservice/technology/technology_microservice/domain/TechnologyUseCaseTest.java
@@ -32,6 +32,60 @@ public class TechnologyUseCaseTest {
     private TechnologyUseCase technologyUseCase;
 
 
+    @Test
+    void createTechnologyTest(){
+        TechnologyModel newTechnology = new TechnologyModel(1L, "React", "Frontend Framework");
+
+        Mockito.when(technologyPersistencePort.existTechnologyByName(newTechnology.getName())).thenReturn(Mono.just(false));
+        Mockito.when(technologyPersistencePort.saveTechnology(newTechnology)).thenReturn(Mono.empty());
+
+        Mono<Void> result = technologyUseCase.createTechnology(newTechnology);
+
+        StepVerifier.create(result).verifyComplete();
+
+        Mockito.verify(technologyPersistencePort, times(1)).existTechnologyByName("React");
+        Mockito.verify(technologyPersistencePort, times(1)).saveTechnology(newTechnology);
+    }
+
+    @Test
+    void createTechnologyTest_shoulThrowAlreadyExistsException(){
+        TechnologyModel newTechnology = new TechnologyModel(1L, "React", "Frontend Framework");
+
+        Mockito.when(technologyPersistencePort.existTechnologyByName(newTechnology.getName())).thenReturn(Mono.just(true));
+
+        Mono<Void> result = technologyUseCase.createTechnology(newTechnology);
+
+        StepVerifier.create(result).expectError(AlreadyExistsException.class).verify();
+
+        Mockito.verify(technologyPersistencePort, times(1)).existTechnologyByName("React");
+        Mockito.verify(technologyPersistencePort, never()).saveTechnology(newTechnology);
+    }
+
+    @Test
+    void createTechnologyTest_shoulThrowNameTooLongException(){
+        String longName = "React".repeat(100);
+        TechnologyModel newTechnology = new TechnologyModel(1L, longName, "Frontend Framework");
+
+        Mockito.when(technologyPersistencePort.existTechnologyByName(newTechnology.getName())).thenReturn(Mono.just(false));
+        Mono<Void> result = technologyUseCase.createTechnology(newTechnology);
+
+        StepVerifier.create(result).expectError(NameTooLongException.class).verify();
+
+        Mockito.verify(technologyPersistencePort, never()).saveTechnology(Mockito.any());
+    }
+
+    @Test
+    void createTechnologyTest_shoulThrowDescriptionTooLongException(){
+        String longDescription = "Frontend Framework".repeat(100);
+        TechnologyModel newTechnology = new TechnologyModel(1L, "React", longDescription);
+
+        Mockito.when(technologyPersistencePort.existTechnologyByName(newTechnology.getName())).thenReturn(Mono.just(false));
+        Mono<Void> result = technologyUseCase.createTechnology(newTechnology);
+
+        StepVerifier.create(result).expectError(DescriptionTooLongException.class).verify();
+
+        Mockito.verify(technologyPersistencePort, never()).saveTechnology(Mockito.any());
+    }
 
     @Test
     void listAllTechnologies_shouldReturnPaginatedData(){
